c0af5c59a851f79dfaa428ac856906f627ca3418
Toby Slight <tslight@pm.me>
2023-10-07 09:24:42 +0100

    formatting

--- shell.nix
+++ shell.nix
@@ -1,14 +1,14 @@
 { pkgs ? (
-    let
-      inherit (builtins) fetchTree fromJSON readFile;
-      inherit ((fromJSON (readFile ./flake.lock)).nodes) nixpkgs gomod2nix;
-    in
+  let
+    inherit (builtins) fetchTree fromJSON readFile;
+    inherit ((fromJSON (readFile ./flake.lock)).nodes) nixpkgs gomod2nix;
+  in
     import (fetchTree nixpkgs.locked) {
       overlays = [
         (import "${fetchTree gomod2nix.locked}/overlay.nix")
       ];
     }
-  )
+)
 , mkGoEnv ? pkgs.mkGoEnv
 , gomod2nix ? pkgs.gomod2nix
 }:
@@ -17,8 +17,5 @@ let
   goEnv = mkGoEnv { pwd = ./.; };
 in
 pkgs.mkShell {
-  packages = [
-    goEnv
-    gomod2nix
-  ];
+  packages = [goEnv gomod2nix];
 }
